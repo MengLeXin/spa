b5aac3d48cf191fe48cd42cab7b1d7ae189b0362
wangding <408542507@qq.com>
2019-04-14 21:50:47 +0800

    index.html layout.css main.js

---
+++ 10-timer-button/main.js
@@ -0,0 +1,20 @@
+$(function(){
+  var n = 6;
+  var $btnAgree = $('input[type="button"]');
+  $btnAgree.attr('disabled','disabled');
+
+  var timer = window.setInterval(function(){
+    n--;
+    if(n === 0){
+      window.clearInterval(timer);
+      $btnAgree.removeAttr('disabled');
+      $btnAgree.val('同意');
+    }else{
+      $btnAgree.val('同意('+ n + 's)');
+    }
+  },1000);
+
+  $btnAgree.click(function(){
+    alert('就知道你会同意的！');
+  });
+});
